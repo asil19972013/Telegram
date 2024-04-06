c1b318394ad5b67c7c353b6241c5f13573c11fbc
asliddin <asliddinxazratkulov83@gmail.com>
2024-04-06 17:55:07 +0500

    I chanched

--- src/uz/pdp/telegram/backend/model/User.java
+++ src/uz/pdp/telegram/backend/model/User.java
@@ -20,4 +20,20 @@ public class User extends BaceModel {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
