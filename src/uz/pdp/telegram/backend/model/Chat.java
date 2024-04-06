87d4e33795f6295ebd6ef494dd91e16dbd9a65ff
asliddin <asliddinxazratkulov83@gmail.com>
2024-04-06 18:03:40 +0500

    I added chat

---
+++ src/uz/pdp/telegram/backend/model/Chat.java
@@ -0,0 +1,9 @@
+package uz.pdp.telegram.backend.model;
+
+import uz.pdp.telegram.backend.BaceModel;
+
+public class Chat extends BaceModel {
+    private String user1Id;
+
+    private String user2Id;
+}
