1f9be7af51aee6c54b1a5c92eaf4a788dc8f119f
asliddin <asliddinxazratkulov83@gmail.com>
2024-04-08 21:53:35 +0500

    I created LoginViews and views package

---
+++ src/uz/pdp/telegram/ui/FrontEnd.java
@@ -0,0 +1,10 @@
+package uz.pdp.telegram.ui;
+
+public class FrontEnd {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Telegram");
+        while (true){
+
+        }
+    }
+}
---
+++ src/uz/pdp/telegram/ui/views/LoginViews.java
@@ -0,0 +1,4 @@
+package uz.pdp.telegram.ui.views;
+
+public class LoginViews {
+}
