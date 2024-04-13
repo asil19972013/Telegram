29cc85cbfb4075765c4481b78c7bf5b112727325
Xushnud <xushnudxurramov9@gmail.com>
2024-04-13 18:34:21 +0500

    i changed again

--- src/uz/pdp/telegram/ui/views/UserView.java
+++ src/uz/pdp/telegram/ui/views/UserView.java
@@ -1,10 +1,15 @@
 package uz.pdp.telegram.ui.views;
 
+import uz.pdp.telegram.backend.model.Chat;
 import uz.pdp.telegram.backend.model.User;
+import uz.pdp.telegram.backend.service.chatService.ChatService;
+import uz.pdp.telegram.backend.service.chatService.ChatServiceImpl;
 import uz.pdp.telegram.backend.service.userService.UserService;
 import uz.pdp.telegram.backend.service.userService.UserServiceImpl;
 import uz.pdp.telegram.ui.FrontEnd;
 import uz.pdp.telegram.ui.utils.ScanUtil;
+
+import java.util.List;
 
 public class UserView {
     public static int menu(){
@@ -17,9 +22,11 @@ public class UserView {
         return ScanUtil.intScan("Choose: ");
     }
 
+    static ChatService chatService = ChatServiceImpl.getInstance();
 
     public static void userProfile() {
         while (true) {
+            ChatView.allChats();
             int option = menu();
             switch (option) {
                 case 1 -> ChatView.chatPart();
@@ -31,4 +38,8 @@ public class UserView {
         }
 
     }
+
+
+
+
 }
